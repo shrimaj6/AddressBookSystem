a2e7f0ccbe157153318e25741c35fe8c638a50c8
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-16 22:49:21 +0530

    Added AddressBook UC-1

--- Program.cs
+++ Program.cs
@@ -4,9 +4,39 @@ namespace AddressBookSystem
 {
     class Program
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public int ZipCode { get; set; }
+
+        public double PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public void displayContact()
+        {
+            Console.WriteLine(this.FirstName + " " + this.LastName + " " + this.City + " " + this.Address + " " + this.State + " " + this.ZipCode + " " + this.PhoneNumber + " " + this.Email);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBook Programs!");
+            Console.WriteLine("Enter Firstname,LastName,Address,City,State,Zipcode,Phone,Email\n");
+
+
+            Program obj = new Program()//object initializer
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                ZipCode = Convert.ToInt32(Console.ReadLine()),
+                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                Email = Console.ReadLine(),
+
+            };
+            obj.displayContact();
         }
     }
 }
